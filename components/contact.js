1d7f3159ecf91da9eabb92bad2e1318c55bbd7e0
Matt <matt@randomforestweb.com>
2024-01-16 07:50:36 +0900

    contact email

--- components/contact.js
+++ components/contact.js
@@ -4,19 +4,19 @@ customElements.define('contact-component', class extends HTMLElement {
     }
 
     static get observedAttributes() {
-        return ['title', 'text1', 'text2'];
+        return ['header', 'text1', 'text2'];
     }
 
-    attributeChangedCallback(title, newTitle, oldTitle, text1, newText1, oldText1, text2, newText2, oldText2) {
+    attributeChangedCallback(header, newHeader, oldHeader, text1, newText1, oldText1, text2, newText2, oldText2) {
         this.innerHTML = `
             <section class="container">
                 <section class="container-header">
-                    <h2>${newTitle}</h2>
+                    <h2>${newHeader}</h2>
                 </section>
                 <div class="contact-links-text">
                     <contactlinks-component class="contact-links"></contactlinks-component>
                     <div class="text">
-                        <div<p>${newText1}</p></div>
+                        <div<p class="change-contact-text">${newText1}</p></div>
                         <div<p>${newText2}</p></div>
                     </div>
                 </div>
@@ -25,19 +25,19 @@ customElements.define('contact-component', class extends HTMLElement {
     }
 
     connectedCallback() {
-        const title = this.getAttribute('title');
+        const header = this.getAttribute('header');
         const text1 = this.getAttribute('text1');
         const text2 = this.getAttribute('text2');
         
         this.innerHTML = `
             <section class="container">
                 <section class="container-header">
-                        <h2>${title}</h2>
+                        <h2>${header}</h2>
                 </section>
                 <div class="contact-links-text">
                     <contactlinks-component class="contact-links"></contactlinks-component>
                     <div class="contact-text">
-                        <div><p>${text1}</p></div>
+                        <div><p class="change-contact-text">${text1}</p></div>
                         <div><p>${text2}</p></div>
                     </div>
                 </div>
@@ -55,8 +55,26 @@ customElements.define('contactlinks-component', class extends HTMLElement {
                 <li><a href="https://www.instagram.com/busy_bee_bouquets/" aria-label="Instagram"><i class="fa-brands fa-instagram"></i></a></li>
                 <li><a href="https://api.whatsapp.com/send?phone=18055039770" aria-label="Whatsapp"><i class="fa-brands fa-whatsapp"></i></a></li>
                 <li><a href="tel:+18055039770"><i class="fa-solid fa-mobile-screen-button" aria-label="Phone"></i></a></li>
-                <li><a href="mailto:blasingamebusiness@gmail.com"><i class="fa-regular fa-envelope" aria-label="Email"></i></a></li>
+                <li><a href="mailto:blasingamebusiness&commat;gmail&period;com"><i class="fa-regular fa-envelope" aria-label="Email"></i></a></li>
             </ul>
+        `;
+    }
+});
+customElements.define('email-component', class extends HTMLElement {
+    constructor() {
+        super();
+
+        this.innerHTML = `
+            <a class="contact-email" href='mailto:blasingamebusiness&commat;gmail&period;com'><span class="remove-email">HERE</span><span class="remove-email">blasingamebusiness&commat;gmail&period;com</span></a>
+        `;
+    }
+});
+customElements.define('removeat-component', class extends HTMLElement {
+    constructor() {
+        super();
+        
+        this.innerHTML = `
+            <span>at</span>
         `;
     }
 });
--- components/header.js
+++ components/header.js
@@ -94,7 +94,7 @@ customElements.define('hflinks-component', class extends HTMLElement {
             <ul class="hf-links">
                 <li><a aria-label="link to Busy Bee Bouquets Instagram" href="https://www.instagram.com/busy_bee_bouquets/"><i class="fa-brands fa-instagram"></i></a></li>
                 <li><a aria-label="Busy Bee Bouquets phone number" href="tel:+18055039770"><i class="fa-solid fa-mobile-screen-button"></i></a></li>
-                <li><a aria-label="Busy Bee Bouquets email" href="mailto:blasingamebusiness@gmail.com"><i class="fa-regular fa-envelope"></i></a></li>
+                <li><a aria-label="Busy Bee Bouquets email" href="mailto:blasingamebusiness&commat;gmail&period;com"><i class="fa-regular fa-envelope"></i></a></li>
             </ul>
         `;
     }
